f843fcc587281aa2ec8d6a77f9909812c73d6329
Nafis Raihan <nafisraihan@Nafiss-MacBook-Pro.local>
2025-11-18 20:18:49 +0600

    problem-4 done

--- solution.ts
+++ solution.ts
@@ -12,3 +12,53 @@ function formatValue(value: AnyType): AnyType {
     return value;
 }
 
+
+type LengthType = string | number[];
+
+function getLength(value: LengthType): number {
+    if (typeof value === 'string') {
+        return value.length;
+    } else if (Array.isArray(value)) {
+        return value.length;
+    }
+    return 0;
+}
+
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    getDetails(): string {
+        return `Name: ${this.name}, Age: ${this.age}`;
+    }
+}
+
+const person1 = new Person('John Doe', 30);
+console.log(person1.getDetails());
+
+const person2 = new Person('Alice', 25);
+console.log(person2.getDetails());
+
+
+type BookType = {
+    title: string;
+    rating: number;
+}
+
+
+function filterByRating(books: BookType[]): BookType[] {
+  return books.filter(book => book.rating >= 4);
+}
+
+const books: BookType[] = [
+  { title: 'Book A', rating: 4.5 },
+  { title: 'Book B', rating: 3.2 },
+  { title: 'Book C', rating: 5.0 },
+];
+
+console.log(filterByRating(books));
