df30eac075796a00e154e282cce7f66eae8bb8b6
Nafis Raihan <nafisraihan@Nafiss-MacBook-Pro.local>
2025-11-18 15:51:10 +0600

    first problem done

--- solution.ts
+++ solution.ts
@@ -1,1 +1,14 @@
-//problem-1 
+type AnyType = string | number | boolean;
+
+function formatValue(value: AnyType): AnyType {
+    if (typeof value === 'string'){
+        return value.toUpperCase();
+    } else if (typeof value === 'number'){
+        return value*10;
+    } else if (typeof value === 'boolean'){
+        return !value;
+    }
+    
+    return value;
+}
+
